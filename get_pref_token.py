538c5e2a84d2fc598d0e59863bc3b6e32ea86db4
drpraveenkumarm <79145894+drpraveenkumarm@users.noreply.github.com>
2022-05-23 21:46:08 +0530

    Create get_pref_token.py

---
+++ get_pref_token.py
@@ -0,0 +1,30 @@
+def get_pref_token(string, version = "v1_6_5_patch_1"):
+    url = "https://preview.nferx.com/nucleus-api/v2/get_entities_for_token?fold_related=1&token_info=attributes%2Curl%2Chighest_priority_type%2Csources_claiming_synonym&token_sources=1&?"
+    params = {
+        "token": string,
+        "version" : version
+    }
+    x = requests.get(url, params=params, auth = authentication)
+    df = pd.json_normalize(x.json()["result"])
+    lst = []
+    if len(df["data"][0])>=1:
+        lst.append(df["query_token"][0])
+        lst.append(df["data"][0][0]["PREFERRED_TOKEN"])
+        return lst
+    else:
+        lst.append(string)
+        lst.append("No Result")
+        return lst
+
+get_pref_token("atezolizumab")
+
+
+f = open("antibody_FDA_approved.txt","r")
+list_tokens = f.read().split()
+
+list_output = []
+for i in range(0,len(list_tokens)):
+    list_iter = get_pref_token(list_tokens[i], version="v1_6_4_patch_1")
+    list_output.append(list_iter)
+
+pd.DataFrame(list_output,columns=["Search_Token","Pref_Name"]).to_csv("Therapeutic_antibody.csv")
